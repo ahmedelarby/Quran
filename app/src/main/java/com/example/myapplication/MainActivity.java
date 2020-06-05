08c763e26fb0f7787ca54f6a28701aff82e28670
ahmedelarby <62482283+ahmedelarby@users.noreply.github.com>
2020-06-05 18:43:54 +0200

    first Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class  MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "ahmed", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+}
